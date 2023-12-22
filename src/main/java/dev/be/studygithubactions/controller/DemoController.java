c4fdf8ddf026a20c8002035d92fd22dda37bf009
jimmy <dev.sarasa.0310@gmail.com>
2023-12-22 21:07:49 +0900

    Add DemoController

---
+++ src/main/java/dev/be/studygithubactions/controller/DemoController.java
@@ -0,0 +1,14 @@
+package dev.be.studygithubactions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+
+}
