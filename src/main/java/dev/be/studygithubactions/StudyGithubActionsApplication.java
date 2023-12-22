b15f71030b49777508193979ad596e720de3a3c2
jimmy <dev.sarasa.0310@gmail.com>
2023-12-22 18:27:39 +0900

    project setup & add appspec.yml

---
+++ src/main/java/dev/be/studygithubactions/StudyGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package dev.be.studygithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/studygithubactions/StudyGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.studygithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
